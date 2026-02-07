f9d1c15a282d63eeacc2a91e8dfd3910405332f8
HritwikEkade <ekadehritwik@gmail.com>
2026-02-07 15:20:22 -0500

    level 4

--- game/js/levels.js
+++ game/js/levels.js
@@ -3,71 +3,71 @@
 const LEVELS = {
   platformsAndGoals: [
     // Testing level - simple platform and goal
-    // Level 1 - Introduction
-    // {
-    //   platforms: [
-    //     { x: 0, y: 200, w: 200, h: 50 },
-    //     { x: 250, y: 400, w: 150, h: 20, hasIcicles: true },
-    //     { x: 450, y: 320, w: 120, h: 20, hasIcicles: true },
-    //     { x: 620, y: 200, w: 180, h: 50, hasIcicles: true },
-    //   ],
-    //   goal: { x: 680, y: 130, w: 35, h: 35 },
-    //   collectibles: [
-    //     { x: 270, y: 360, w: 20, h: 20 },
-    //     { x: 480, y: 280, w: 20, h: 20 },
-    //   ],
-    // },
+    Level 1 - Introduction
+    {
+      platforms: [
+        { x: 0, y: 200, w: 200, h: 50 },
+        { x: 250, y: 400, w: 150, h: 20, hasIcicles: true },
+        { x: 450, y: 320, w: 120, h: 20, hasIcicles: true },
+        { x: 620, y: 200, w: 180, h: 50, hasIcicles: true },
+      ],
+      goal: { x: 680, y: 130, w: 35, h: 35 },
+      collectibles: [
+        { x: 270, y: 360, w: 20, h: 20 },
+        { x: 480, y: 280, w: 20, h: 20 },
+      ],
+    },
 
-    // // Level 2 - Stepping up
-    // {
-    //   platforms: [
-    //     { x: 0, y: 200, w: 200, h: 50 },
-    //     { x: 200, y: 580, w: 450, h: 20, hasIcicles: true },
-    //     { x: 395, y: 0, w: 10, h: 500, hasIcicles: true },
-    //     { x: 405, y: 480, w: 100, h: 20, hasIcicles: true },
-    //     { x: 550, y: 380, w: 100, h: 20, hasIcicles: true },
-    //     { x: 405, y: 280, w: 100, h: 20, hasIcicles: true },
-    //     { x: 650, y: 250, w: 10, h: 350, hasIcicles: true },
-    //     { x: 650, y: 200, w: 150, h: 50 },
-    //   ],
-    //   goal: { x: 700, y: 130, w: 35, h: 35 },
-    //   collectibles: [
-    //     { x: 435, y: 450, w: 20, h: 20 },
-    //     { x: 580, y: 350, w: 20, h: 20 },
-    //     { x: 435, y: 250, w: 20, h: 20 },
-    //   ],
-    // },
+    // Level 2 - Stepping up
+    {
+      platforms: [
+        { x: 0, y: 200, w: 200, h: 50 },
+        { x: 200, y: 580, w: 450, h: 20, hasIcicles: true },
+        { x: 395, y: 0, w: 10, h: 500, hasIcicles: true },
+        { x: 405, y: 480, w: 100, h: 20, hasIcicles: true },
+        { x: 550, y: 380, w: 100, h: 20, hasIcicles: true },
+        { x: 405, y: 280, w: 100, h: 20, hasIcicles: true },
+        { x: 650, y: 250, w: 10, h: 350, hasIcicles: true },
+        { x: 650, y: 200, w: 150, h: 50 },
+      ],
+      goal: { x: 700, y: 130, w: 35, h: 35 },
+      collectibles: [
+        { x: 435, y: 450, w: 20, h: 20 },
+        { x: 580, y: 350, w: 20, h: 20 },
+        { x: 435, y: 250, w: 20, h: 20 },
+      ],
+    },
 
-    // // Level 3 - Icicle Danger Zone
-    // {
-    //   platforms: [
-    //     { x: 0, y: 200, w: 150, h: 50 },
-    //     { x: 200, y: 350, w: 120, h: 20, hasIcicles: true },
-    //     { x: 370, y: 280, w: 100, h: 20, hasIcicles: true },
-    //     { x: 520, y: 400, w: 100, h: 20, hasIcicles: true },
-    //     { x: 370, y: 480, w: 100, h: 20, hasIcicles: true },
-    //     { x: 650, y: 300, w: 150, h: 50, hasIcicles: true },
-    //   ],
-    //   goal: { x: 690, y: 230, w: 35, h: 35 },
-    //   collectibles: [
-    //     { x: 230, y: 310, w: 20, h: 20 },
-    //     { x: 550, y: 360, w: 20, h: 20 },
-    //   ],
-    //   icicles: [
-    //     // Icicles hanging from ceiling - avoid these!
-    //     { x: 180, y: 0, w: 20, h: 60 }, // Above first gap
-    //     { x: 340, y: 0, w: 25, h: 80 }, // Above second platform
-    //     { x: 490, y: 0, w: 20, h: 70 }, // Between platforms
-    //     { x: 600, y: 0, w: 30, h: 90 }, // Before final platform
-    //     { x: 250, y: 0, w: 18, h: 50 }, // Extra danger
-    //     { x: 420, y: 0, w: 22, h: 65 }, // Extra danger
-    //   ],
-    //   polarBears: [
-    //     // Polar bears that patrol horizontally - avoid these!
-    //     { x: 200, y: 310, w: 40, h: 35, minX: 200, maxX: 280, speed: 1 }, // On second platform
-    //     { x: 520, y: 360, w: 40, h: 35, minX: 520, maxX: 600, speed: 1 }, // On fourth platform
-    //   ],
-    // },
+    // Level 3 - Icicle Danger Zone
+    {
+      platforms: [
+        { x: 0, y: 200, w: 150, h: 50 },
+        { x: 200, y: 350, w: 120, h: 20, hasIcicles: true },
+        { x: 370, y: 280, w: 100, h: 20, hasIcicles: true },
+        { x: 520, y: 400, w: 100, h: 20, hasIcicles: true },
+        { x: 370, y: 480, w: 100, h: 20, hasIcicles: true },
+        { x: 650, y: 300, w: 150, h: 50, hasIcicles: true },
+      ],
+      goal: { x: 690, y: 230, w: 35, h: 35 },
+      collectibles: [
+        { x: 230, y: 310, w: 20, h: 20 },
+        { x: 550, y: 360, w: 20, h: 20 },
+      ],
+      icicles: [
+        // Icicles hanging from ceiling - avoid these!
+        { x: 180, y: 0, w: 20, h: 60 }, // Above first gap
+        { x: 340, y: 0, w: 25, h: 80 }, // Above second platform
+        { x: 490, y: 0, w: 20, h: 70 }, // Between platforms
+        { x: 600, y: 0, w: 30, h: 90 }, // Before final platform
+        { x: 250, y: 0, w: 18, h: 50 }, // Extra danger
+        { x: 420, y: 0, w: 22, h: 65 }, // Extra danger
+      ],
+      polarBears: [
+        // Polar bears that patrol horizontally - avoid these!
+        { x: 200, y: 310, w: 40, h: 35, minX: 200, maxX: 280, speed: 1 }, // On second platform
+        { x: 520, y: 360, w: 40, h: 35, minX: 520, maxX: 600, speed: 1 }, // On fourth platform
+      ],
+    },
 
     // Level 4 - Icicle Gauntlet (HARD - Heavy Icicle Focus)
     {
